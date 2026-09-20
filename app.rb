d0e51388a5ffc39dd7b63e328b1ea2df2e9f5681
madeline-waig <madeline@MADELINE.localdomain>
2026-09-20 13:59:26 -0700

    Done Cucumbers and RSpec Tests

--- app.rb
+++ app.rb
@@ -39,8 +39,19 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
+    letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    
+    begin 
+      result = @game.guess(letter) #new letter 
+      if result == false
+        flash[:message] = "You have already used that letter."
+      end 
+
+    rescue ArgumentError #cannot use == :invalid since the output never invalid
+      flash[:message] = "Invalid guess."
+    end 
+
     redirect '/show'
   end
 
@@ -51,16 +62,25 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    status = @game.check_win_or_lose
+    if status == :win
+      redirect '/win'
+    elsif status == :lose
+      redirect '/lose'
+    else 
+      erb :show # You may change/remove this line
+    end 
   end
 
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :win
     erb :win # You may change/remove this line
   end
 
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :lose
     erb :lose # You may change/remove this line
   end
 end
